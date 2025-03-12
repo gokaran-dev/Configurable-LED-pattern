efb01048053a671ef12bb44b41f5f8d22136431c
gokaran-dev <gokaran.gautam24m@iiitg.ac.in>
2025-03-12 23:21:00 +0530

    Initial commit for reconfigurable LED pattern project

---
+++ Configurable Pattern Light.srcs/sim_1/new/weddinglight_3TB.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module weddinglight_3TB();
+        
+        reg [4:0]load;
+        reg clk,rst;
+        wire [15:0]q;
+       // wire [1:0]j;
+        //wire [2:0]load_count;
+
+ 
+
+        weddinglight_3 uut(.clk(clk),.load(load),.rst(rst),.q(q));
+        
+        initial
+            begin
+                load=5'b00000;
+                clk=0;
+                rst=0;
+                
+             end
+             
+          always
+            #5 clk=~clk;
+            
+            initial
+                begin
+                    #100
+                    load=5'b01111;
+                    #300
+                    load= 5'b00000;
+                    #10
+                    rst=1;
+                    #500
+                    rst=0;
+                   // #10 load=5'b10101;
+                    #1000
+                    #100 $finish;
+                 end
+                    
+endmodule
